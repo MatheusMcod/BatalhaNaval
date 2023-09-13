60f6311f8abfcd3cec21d9517ddc8da4a1f05d6e
Matheus Moreira Costa <90786402+MatheusMcod@users.noreply.github.com>
2023-09-13 15:14:45 -0300

    :art: - Melhorando a estrutura do projeto com router e padroes

---
+++ src/componentes/Tabuleiro/index.jsx
@@ -0,0 +1,10 @@
+//import styles from './tabuleiroStyle.module.css'
+
+function GameGride() {
+
+    return (
+        <p>Ok</p>
+    );
+}
+
+export default GameGride;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Body from '../componentes/BodyHome/BodyHome'
+import Header from '../componentes/Header/Header'
+
+function Home() {
+
+    return (
+        <>
+            <Header/>
+            <Body/>
+        </>
+    );
+}
+
+export default Home;
