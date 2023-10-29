33adad6aefb8a75f1f93212819c0c3d1c2089717
Matheus Moreira Costa <90786402+MatheusMcod@users.noreply.github.com>
2023-10-29 17:27:41 -0300

    :sparkles: Sistema de Rotas

--- src/api/index.php
+++ src/api/index.php
@@ -1,5 +1,14 @@
 <?php
-include("phptest.php");
 
-$test=new phptest();
-$test->exibir();
+require_once __DIR__.'/core/Core.php';
+require_once __DIR__.'/routes/routes.php';
+
+spl_autoload_register(function($file) {
+    if(file_exists(__DIR__."/src/api/models/$file.php")) {
+        require_once __DIR__."/models/$file.php";
+    }
+});
+
+$core = new Core();
+$core->run($routes);
+
