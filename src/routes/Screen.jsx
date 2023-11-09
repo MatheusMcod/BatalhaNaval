8f726ff95a7cfd8c4749818f0c1de49cc54290b7
EDUARDO ALVES MARTINS NETO <108354728+Eduardo6k@users.noreply.github.com>
2023-11-09 14:02:55 -0300

    update

---
+++ src/routes/Screen.jsx
@@ -0,0 +1,11 @@
+import ScreenBoard from "../componentes/Screen/ScreenBoard"
+
+ function screen(){
+    return( 
+     <>
+        <ScreenBoard/>
+      </>
+      );
+   
+}
+export default screen;
